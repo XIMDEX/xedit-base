221b0c9e39c6e2f6eef1b9bb778d1c3145f11783
David Arroyo <arroyoescobardavid@gmail.com>
2018-08-22 08:47:24 +0200

    Added get method in Ixedit interface

--- src/Interfaces/Models/IXedit.php
+++ src/Interfaces/Models/IXedit.php
@@ -7,6 +7,9 @@ use Xedit\Base\Core\IBase;
 interface IXedit extends IBase
 {
 
+    /** Methods */
+    public static function get($condition): IXedit;
+
     /** Relations */
     public function getContainer(): IXeditContainer;
 
